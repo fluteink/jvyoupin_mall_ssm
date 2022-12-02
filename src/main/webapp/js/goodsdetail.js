21547f857faab188a9af8784b819cc0a57b04851
fluteink <fluteink@qq.com>
2022-12-02 19:15:41 +0800

    更新代码

--- src/main/webapp/js/goodsdetail.js
+++ src/main/webapp/js/goodsdetail.js
@@ -6,18 +6,16 @@ var vm = new Vue({
             var pid = window.location.href;
             var p = new Array();
             p = pid.split('/');
-            console.log(p);
             pid = p[6];
-            console.log("666")
-            console.log(pid)
             pid = parseInt(pid);
-            console.log(pid)
+            console.log(pid);
             // pid = pid.substring(pid.indexOf('goodsdetail/') + 12, pid.length);
             axios.post("/jvyoupin/findproduct/" + pid).then(response => {
                 // this.goods = $.parseJSON(response.data);
                 this.goods = response.data;
                 console.log(this.goods);
             });
+
         }
     },
     data() {
