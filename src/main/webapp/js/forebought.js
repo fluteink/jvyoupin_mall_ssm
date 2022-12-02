cdf7e84a8933495e0d4be0aa90eb1b74cc253961
fluteink <fluteink@qq.com>
2022-12-02 22:14:21 +0800

    更新代码

---
+++ src/main/webapp/js/forebought.js
@@ -0,0 +1,32 @@
+var order;
+var vue = new Vue({
+    el: "#order",
+    methods: {
+        getorder() {
+            var uid = window.location.href;
+            // uid = uid.substring(uid.indexOf('forebought/') + 10, uid.length);
+            var p = new Array();
+            p = uid.split('/');
+            console.log(p);
+            uid = p[5];
+            uid = parseInt(uid);
+            console.log(uid);
+
+
+            axios.post("/jvyoupin/findorderItem/" + uid).then(response => {
+
+                this.order = response.data;
+                console.log(this.order)
+            });
+        }
+    },
+    data() {
+        return {
+            order: []
+        }
+
+    },
+    created: function () {
+        this.getorder()
+    }
+});
